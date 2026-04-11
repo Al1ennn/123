61833a260148529bd38bd64411d56f45d63be290
Al1ennn <146171098+Al1ennn@users.noreply.github.com>
2026-04-11 19:19:30 +0300

    Delete game2/enemies.js

--- game2/enemies.js
+++
@@ -1,8 +0,0 @@
-// Нові координати для центру нової карти
-const enemiesList = [
-    { x: TILE_SIZE * 9.5, y: TILE_SIZE * 7.5, color: '#ff0000', dir: 'up', size: TILE_SIZE * 0.8 },
-    { x: TILE_SIZE * 10.5, y: TILE_SIZE * 7.5, color: '#ff8800', dir: 'left', size: TILE_SIZE * 0.8 },
-    { x: TILE_SIZE * 11.5, y: TILE_SIZE * 7.5, color: '#aa00ff', dir: 'right', size: TILE_SIZE * 0.8 }
-];
-
-const possibleDirections = ['up', 'down', 'left', 'right'];
